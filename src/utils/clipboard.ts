9d06b45d685ef36c43c18999590de48ce5422474
ianzhi <ianzhi@126.com>
2023-06-13 13:05:58 +0800

    build: 完善复制到剪贴板功能

--- src/utils/clipboard.ts
+++ src/utils/clipboard.ts
@@ -1,17 +1,27 @@
 export default async function copy(text: string) {
-    if (navigator.clipboard) {
-        await navigator.clipboard.writeText(text)
-        return true
-    } else {
-        var input = document.createElement('textarea')
-        input.value = text
-        document.body.appendChild(input)
-        input.select()
+    var input = document.createElement('textarea')
+    input.value = text
+    document.body.appendChild(input)
+    input.select()
 
-        // 复制
-        const result = document.execCommand('copy', true)
-        document.body.removeChild(input)
+    // 复制
+    const result = document.execCommand('copy', true)
+    document.body.removeChild(input)
 
+    if (result) {
         return result
+    }
+
+    if (!navigator.clipboard) {
+        return false
+    }
+
+    try {
+        await navigator.clipboard.writeText(text)
+
+        return true
+    } catch (e) {
+        console.error(e)
+        return false
     }
 }
