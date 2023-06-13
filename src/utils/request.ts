a7ec4a26753349165ee6de40a6b3ab88e587ba7e
ianzhi <ianzhi@126.com>
2023-06-13 18:44:23 +0800

    build: 完善自动退出登陆功能

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,6 @@
+import router from '@/router'
 import { useAuthStore } from '@/stores/auth'
-import axios from 'axios'
+import axios, { AxiosError, type AxiosResponse } from 'axios'
 
 const request = axios.create({
     baseURL: import.meta.env.DEV ? '/wp-json' : 'https://api.xyyb666.com/wp-json',
@@ -18,6 +19,16 @@ request.interceptors.request.use(config => {
     }
 
     return config
+})
+
+request.interceptors.response.use(res => res, (err: AxiosError) => {
+    if (err.response && err.response.status === 403) {
+        const authStore = useAuthStore()
+        authStore.logout()
+        router.replace({ name: 'login' })
+    } else {
+        return err
+    }
 })
 
 export default request
--- src/views/renmai/MineRenmai.vue
+++ src/views/renmai/MineRenmai.vue
@@ -1,13 +1,26 @@
 <template>
     <div class="mine-renmai">
         <van-cell-group inset style="margin-top: var(--van-padding-md)">
-            <van-cell
-                v-for="(i, index) in renmai_count"
-                :title="`${convertToChinaNum(index + 1)}环人脉`"
-                :key="index"
-                :value="`${i}人`"
-                :to="{ name: 'mine.renmai.people', params: { level: index + 1 } }"
-                is-link />
+            <template v-for="(i, index) in renmai_count">
+                <van-cell
+                    v-if="profile && profile.meta && (index >= profile.meta.qianmai_level)"
+                    :title="`${convertToChinaNum(index + 1)}环人脉`"
+                    :key="`disable-${index}`">
+                    <template #right-icon>
+                        <van-icon name="lock" />
+                    </template>
+                </van-cell>
+                <van-cell
+                    v-else
+                    :title="`${convertToChinaNum(index + 1)}环人脉`"
+                    :key="`normal-${index}`"
+                    :to="{ name: 'mine.renmai.people', params: { level: index + 1 } }"
+                    is-link>
+                    <template #value>
+                        {{ i }}人
+                    </template>
+                </van-cell>
+            </template>
         </van-cell-group>
     </div>
 </template>
@@ -16,9 +29,14 @@
 import { onMounted, ref } from 'vue'
 import { getRenmaiCount } from '@/api/auth'
 import { convertToChinaNum } from '@/utils/number'
+import { useAuthStore, type UserProfile } from '@/stores/auth'
 
 const renmai_count = ref<number[]>([])
+const profile = ref<UserProfile | undefined>(undefined)
+
 onMounted(async () => {
+    const authStore = useAuthStore()
+    profile.value = await authStore.me()
     renmai_count.value = await getRenmaiCount()
 })
 </script>
